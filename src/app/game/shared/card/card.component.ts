48343234e2458f4bf136c4be162b9e8b8aeacf12
Szymon Skórka <szymonskorka@outlook.com>
2020-06-04 19:29:30 +0200

    Fix the error when the deck is emptied

--- src/app/game/shared/card/card.component.ts
+++ src/app/game/shared/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Card } from './card.model';
 
 @Component({
@@ -6,15 +6,24 @@ import { Card } from './card.model';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
-  @Input() card: Card = new Card("placeholder", "", false, 0);  // if left undefined, will cause errors
+export class CardComponent implements OnInit, OnChanges {
+  deckPath = "/../../../../assets/images/cards/backs/back-c.png"
+  readonly deckEmptyPath = "/../../../../assets/images/cards/backs/deck-empty.png"
+  readonly placeholderCard: Card = new Card("placeholder", "", false, 0);
+
+  @Input() card: Card = this.placeholderCard;  // if left undefined, will cause errors
   @Input() isDeck = false;
   @Input() isInPot = false;
-
-  deckPath = "/../../../../assets/images/cards/backs/back-c.png"
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    if (!this.card) {
+      this.card = this.placeholderCard;
+      this.deckPath = this.deckEmptyPath;
+    }
   }
 }
