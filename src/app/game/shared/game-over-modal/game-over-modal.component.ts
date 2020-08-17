c9f559d71c5b09dd477582a1013feb83ce8a1513
sskorka <szymonskorka@outlook.com>
2020-08-17 22:36:48 +0200

    Add empty component - game over modal window

---
+++ src/app/game/shared/game-over-modal/game-over-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-over-modal',
+  templateUrl: './game-over-modal.component.html',
+  styleUrls: ['./game-over-modal.component.css']
+})
+export class GameOverModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
