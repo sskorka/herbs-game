13a1c607b98b9b42703b368beddb5e347f42521a
sskorka <szymonskorka@outlook.com>
2021-03-06 13:12:26 +0100

    Update home component
    It will now display app title with proper formatting.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,11 @@ import { AuthService } from '../auth/auth.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  appTitle = environment.APP_NAME;
 
   constructor(private authService: AuthService) { }
 
-  onPlay(): void {
+  onPlay() {
     this.authService.playNowEvent.emit();
   }
 }
