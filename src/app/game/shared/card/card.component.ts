83ffc89185c26f79286e851fb4040298b6faebfb
sskorka <szymonskorka@outlook.com>
2021-03-08 17:02:02 +0100

    Create get method for card image url

--- src/app/game/shared/card/card.component.ts
+++ src/app/game/shared/card/card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
-import { Card } from './card.model';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { environment } from 'src/environments/environment';
+import { Card } from './card';
+import { CARDS } from '../../models/constants/cards';
 
 @Component({
   selector: 'app-card',
@@ -9,9 +12,11 @@ import { Card } from './card.model';
 export class CardComponent {
   deckPath = "/../../../../assets/images/cards/backs/back-c.png"
   readonly deckEmptyPath = "/../../../../assets/images/cards/backs/deck-empty.png"
-  readonly placeholderCard: Card = new Card("placeholder", "", false, 0);
+  readonly placeholderCard: Card = new Card(CARDS.PLACEHOLDER);
 
   private _card: Card;
+  private language: string = environment.DEFAULT_LANGUAGE;
+
   @Input() set card(card: Card) {
     if(!card) {
       this._card = this.placeholderCard;
@@ -23,6 +28,16 @@ export class CardComponent {
   get card(): Card {
     return this._card;
   }
+
   @Input() isDeck = false;
   @Input() isInPot = false;
+
+  public get imageUrl(): string {
+    return `url(${this.card.getImagePath(this.language)})`;
+  }
+
+  constructor(private translate: TranslateService) {
+    this.language = translate.currentLang;
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => this.language = event.lang)
+  }
 }
