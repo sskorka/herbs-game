395083d2929ee6d89392f9173ab925d335476897
sskorka <szymonskorka@outlook.com>
2021-03-17 21:01:51 +0100

    Update production environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  appName: 'Herbs Up!',
+  defaultLanguage: 'en',
+  devMail: 'szymonskorka@outlook.com',
+  firebase: { // replace these with your own before building/serving
+    apiKey: 'AIzaSyBMEkW7zQd6dUCTv23jpmpxPcOWe4Zf6Eo',
+    endpoints: {
+      signin: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
+      signup: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='
+    },
+    db: {
+      default: 'https://herbs-game.firebaseio.com',
+      names: 'https://herbs-game.firebaseio.com/names/',
+      extraData: 'https://herbs-game.firebaseio.com/extradata/'
+    }
+  }
 };
